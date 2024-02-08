ab2ce7bf6ca0f6cbff39c0d7727d18aaa270c4cf
Kczar <kenarnold123213@gmail.com>
2024-02-08 11:46:36 +0800

    Router & pages

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Login from './Component/Login.jsx'
+import App from './App.jsx';
+import GuestLayout from './GuestLayout.jsx';
+import StudentLayout from './StudentLayout.jsx';
+import Dashboard from './Component/Dashboard.jsx';
+import SetAppointment from './Component/SetAppointment.jsx';
+import ViewAppointments from './Component/ViewAppointments.jsx';
 
-function router() {
-  return (
-    <div>router</div>
-  )
-}
-
-export default router
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    },
+    {
+        path: '/guest',
+        element: <GuestLayout />
+    },
+    {
+        path: '/student',
+        element: <StudentLayout />,
+        
+    },
+    {
+        path: '/student/dashboard',
+        element: <Dashboard />,
+        
+    },
+    
+    {
+        path: '/student/set-appointment',
+        element: <SetAppointment />,
+        
+    },
+    
+    {
+        path: '/student/view-appointment',
+        element: <ViewAppointmentsy />,
+        
+    },
+    
+])
+export default router;
