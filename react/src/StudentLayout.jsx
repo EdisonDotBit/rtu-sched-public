14998925ba7e931a1181c39ed56fc388802c83d6
Edison Lati Jr <junior.lati@gmail.com>
2024-02-12 16:10:23 +0800

    Updated Sidebar and Added Footer to StudentLayout.jsx

--- react/src/StudentLayout.jsx
+++ react/src/StudentLayout.jsx
@@ -21,16 +21,16 @@ function StudentLayout() {
                                 >
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
-                                        className="h-6 w-6 mr-2"
                                         fill="none"
                                         viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
                                         stroke="currentColor"
+                                        className="w-6 h-6 mr-2"
                                     >
                                         <path
                                             strokeLinecap="round"
                                             strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            d="M4 6h16M4 12h16M4 18h16"
+                                            d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z"
                                         />
                                     </svg>
                                     Dashboard
@@ -41,16 +41,16 @@ function StudentLayout() {
                                 >
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
-                                        className="h-6 w-6 mr-2"
                                         fill="none"
                                         viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
                                         stroke="currentColor"
+                                        className="w-6 h-6 mr-2"
                                     >
                                         <path
                                             strokeLinecap="round"
                                             strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            d="M6 18L18 6M6 6l12 12"
+                                            d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
                                         />
                                     </svg>
                                     Set Appointment
@@ -61,16 +61,16 @@ function StudentLayout() {
                                 >
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
-                                        className="h-6 w-6 mr-2"
                                         fill="none"
                                         viewBox="0 0 24 24"
+                                        strokeWidth={1.5}
                                         stroke="currentColor"
+                                        className="w-6 h-6 mr-2"
                                     >
                                         <path
                                             strokeLinecap="round"
                                             strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            d="M13 10V3L4 14h7v7l9-11h-7z"
+                                            d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75"
                                         />
                                     </svg>
                                     View Appointment
@@ -123,7 +123,7 @@ function StudentLayout() {
                                 </button>
                             </div>
                         </div>
-                        <div className="p-4">
+                        <div className="p-4 bg-white flex-1 rounded-lg m-4">
                             <h1 className="text-2xl font-bold">
                                 Welcome to my dashboard!
                             </h1>
@@ -134,9 +134,13 @@ function StudentLayout() {
                     </div>
                 </div>
 
-                <div>
-                    <Footer />
-                </div>
+                <footer className="footer flex text-center justify-center text-sm items-center h-12 border-t w-screen px-6 py-1">
+                    <p>
+                        {" "}
+                        Copyright © 2024 - All right reserved by Rizal
+                        Technological University
+                    </p>
+                </footer>
             </div>
         </>
     );
