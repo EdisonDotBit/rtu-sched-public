3266a8b899973a1efdf40653b388c323ac36c51e
Edison Lati Jr <junior.lati@gmail.com>
2024-02-12 14:44:29 +0800

    Update StudentLayout.jsx

--- react/src/StudentLayout.jsx
+++ react/src/StudentLayout.jsx
@@ -1,9 +1,139 @@
-import React from 'react'
+import React from "react";
+import Footer from "./component/subcomponent/footer";
 
 function StudentLayout() {
-  return (
-    <div>StudentLayout</div>
-  )
+    return (
+        <>
+            <div className="flex h-screen bg-gray-100">
+                {/* Sidebar */}
+                <div className="hidden md:flex flex-col w-64 bg-gray-800">
+                    <div className="flex items-center justify-center h-16 bg-gray-900">
+                        <span className="text-white font-bold uppercase">
+                            RTU Online Appointment
+                        </span>
+                    </div>
+                    <div className="flex flex-col flex-1 overflow-y-auto">
+                        <nav className="flex-1 bg-[#194F90]">
+                            <a
+                                href="#"
+                                className="flex items-center px-4 py-6 text-white hover:text-white bg-[#123A69]"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6 mr-2"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                                Dashboard
+                            </a>
+                            <a
+                                href="#"
+                                className="flex items-center px-4 py-6 text-white hover:text-white hover:bg-[#123A69]"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6 mr-2"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                                Set Appointment
+                            </a>
+                            <a
+                                href="#"
+                                className="flex items-center px-4 py-6 text-white hover:text-white hover:bg-[#123A69]"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6 mr-2"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M13 10V3L4 14h7v7l9-11h-7z"
+                                    />
+                                </svg>
+                                View Appointment
+                            </a>
+                        </nav>
+                    </div>
+                </div>
+
+                {/* Main content */}
+                <div className="flex flex-col flex-1 overflow-y-auto">
+                    <div className="flex items-center justify-between h-16 bg-white border-b border-gray-200">
+                        <div className="flex items-center px-4">
+                            <button className="text-gray-500 focus:outline-none focus:text-gray-700">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                        <div>
+                            <h1 className="bold text-xl">Student</h1>
+                        </div>
+                        <div className="flex items-center pr-4">
+                            <button className="flex items-center text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M12 19l-7-7 7-7m5 14l7-7-7-7"
+                                    />
+                                </svg>
+                                Logout
+                            </button>
+                        </div>
+                    </div>
+                    <div className="p-4">
+                        <h1 className="text-2xl font-bold">
+                            Welcome to my dashboard!
+                        </h1>
+                        <p className="mt-2 text-gray-600">
+                            This is an example dashboard using Tailwind CSS.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default StudentLayout
+export default StudentLayout;
