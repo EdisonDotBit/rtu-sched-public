839009ac9dff54f0c45cbd700fd7e1504d01ab8e
Paolo Banaglorioso <93817250+chippee08@users.noreply.github.com>
2024-02-13 22:02:03 +0800

    Content added in SelectBranch.jsx
    Meron nang content, ayusin nalang yung design.

--- react/src/Component/SelectBranch.jsx
+++ react/src/Component/SelectBranch.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import SubNav from './Subcomponent/SubNav'
+import Campus from './Subcomponent/Asset/RTU_Campus.jpg'
 
 function SelectBranch() {
   return (
     <>
-     <div className="">SelectBranch
+     <div className="">
         <div>
             <SubNav />
+            <h1 className="flex justify-center text-[#194F90] text-[1.5rem] font-bold">Select RTU Branch</h1>
         </div>
-        <div>
-            
+        <div className="flex justify-evenly p-[20px]">
+            <div className="border-solid border-[1px] border-black rounded-lg hover:bg-[#d9d9d9]">
+              <button className="p-[20px]">
+                <img src={Campus} alt="" className="rounded-2xl w-[500px] p-[10px]"/>
+                <h2 className="text-[#194F90] text-[1.5rem] font-bold">Rizal Technological University - Boni</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </button>
+            </div>
+            <div className="border-solid border-[1px] border-black rounded-lg hover:bg-[#d9d9d9]">
+              <button className="p-[20px]">
+                <img src={Campus} alt="" className="rounded-2xl w-[500px] p-[10px]"/>
+                <h2 className="text-[#194F90] text-[1.5rem] font-bold">Rizal Technological University - Pasig</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </button>
+            </div>
         </div>
      </div>
     </>
