3d90bd4f3df348641bb20859b52df76591429749
Edison Lati Jr <junior.lati@gmail.com>
2024-02-13 13:41:06 +0800

    Updated SubNav and StudentLayout

--- react/src/StudentLayout.jsx
+++ react/src/StudentLayout.jsx
@@ -24,6 +24,7 @@ function StudentLayout() {
                         </span>
                     </div>
                     <div className="flex flex-col flex-1 overflow-y-auto">
+<<<<<<< Updated upstream
                         <nav className="flex-1 px-2 py-4 bg-gray-800">
                             <a
                                 href="#"
@@ -86,6 +87,69 @@ function StudentLayout() {
                                 View Appointment
                             </a>
                         </nav>
+=======
+                        <div className="flex items-center justify-between h-16 bg-white border-b border-gray-200">
+                            <div className="flex items-center px-4">
+                                <button className="text-gray-500 focus:outline-none focus:text-gray-700">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div>
+                                <h1 className="bold text-xl">Student</h1>
+                            </div>
+                            <div className="flex items-center pr-4">
+                                <button className="flex items-center text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M12 19l-7-7 7-7m5 14l7-7-7-7"
+                                        />
+                                    </svg>
+                                    Logout
+                                </button>
+                            </div>
+                        </div>
+                        <div className="p-4 bg-white flex-1 rounded-lg m-4">
+                            <div className="flex justify-between bg-[#194F90] text-white h-[50px] xl:text-xl sm:text-base">
+                                <button className="bg-[#123A69] flex-1 hover:text-white hover:bg-[#123A69]">
+                                    RTU Branch
+                                </button>
+
+                                <button className="hover:text-white flex-1 hover:bg-[#123A69]">
+                                    Select Office
+                                </button>
+
+                                <button className="hover:text-white flex-1 hover:bg-[#123A69]">
+                                    Purpose, Date & Time
+                                </button>
+
+                                <button className="hover:text-white flex-1 hover:bg-[#123A69]">
+                                    Confirmation
+                                </button>
+                            </div>
+                        </div>
+>>>>>>> Stashed changes
                     </div>
                 </div>
 
