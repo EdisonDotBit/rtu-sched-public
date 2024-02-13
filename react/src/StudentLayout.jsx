2eec334bcc102168d375d02b664ca68429549945
Kczar <kenarnold123213@gmail.com>
2024-02-13 13:32:37 +0800

    AASD

--- react/src/StudentLayout.jsx
+++ react/src/StudentLayout.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function StudentLayout() {
     // State to track whether the sidebar is open or closed
@@ -137,12 +138,7 @@ function StudentLayout() {
                         </div>
                     </div>
                     <div className="p-4">
-                        <h1 className="text-2xl font-bold">
-                            Welcome to my dashboard!
-                        </h1>
-                        <p className="mt-2 text-gray-600">
-                            This is an example dashboard using Tailwind CSS.
-                        </p>
+                        <Outlet />
                     </div>
                 </div>
             </div>
