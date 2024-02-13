ae098b1d9c2f2372649574dde0b8696534d92ea8
Paolo Banaglorioso <93817250+chippee08@users.noreply.github.com>
2024-02-13 13:13:37 +0800

    Update subnavbar

--- react/src/Component/Subcomponent/SubNav.jsx
+++ react/src/Component/Subcomponent/SubNav.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 function SubNav() {
   return (
+    <>
     <div>SubNav</div>
+      <div className="p-4 bg-white flex-1 rounded-lg m-4">
+          <div className="flex justify-around bg-[#194F90] text-white h-[50px] xl:text-xl sm:text-base">
+            <button className="bg-[#123A69] hover:text-white hover:bg-[#123A69] w-[300px]">
+              RTU Branch
+            </button>
+
+            <button className="hover:text-white hover:bg-[#123A69] w-[300px]">
+              Select Office
+            </button>
+
+                                    <button className="hover:text-white hover:bg-[#123A69] w-[300px]">
+                                        Purpose, Date & Time
+                                    </button>
+
+                                    <button className="hover:text-white hover:bg-[#123A69] w-[300px]">
+                                        Confirmation
+                                    </button>
+                            </div>
+                        </div>
+    </>
   )
 }
 
