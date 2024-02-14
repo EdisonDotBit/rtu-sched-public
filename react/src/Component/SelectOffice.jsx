99a3723e35193e83aa028415ee37c7b2aa91fcd9
Paolo Banaglorioso <93817250+chippee08@users.noreply.github.com>
2024-02-14 10:50:47 +0800

    Created SelectOffice.jsx
    Di ko pa alam ano ilalagay lul

---
+++ react/src/Component/SelectOffice.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import SubNav from './Subcomponent/SubNav'
+
+function SelectOffice() {
+  return (
+    <>
+        
+        <div>
+            <div>
+                <SubNav />
+                <h1 className="flex justify-center text-[#194F90] text-[1.9rem] font-bold pb-[30px]">Select Office</h1>
+            </div>
+          
+          <div>
+
+          </div>
+        </div>
+    </>
+    
+  )
+}
+
+export default SelectOffice
