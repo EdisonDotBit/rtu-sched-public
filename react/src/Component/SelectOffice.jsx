1ea191789be1b7795956dce16d66cfb0be19f077
Paolo Banaglorioso <93817250+chippee08@users.noreply.github.com>
2024-02-15 20:43:07 +0800

    Update sa SelecOffice
    Di pa responsive

--- react/src/Component/SelectOffice.jsx
+++ react/src/Component/SelectOffice.jsx
@@ -11,11 +11,10 @@ function SelectOffice() {
                 <h1 className="flex justify-center text-[#194F90] text-[1.9rem] font-bold pb-[30px]">Select Office</h1>
                 
                 <div className="flex justify-evenly self-center flex-wrap gap-[20px] w-[1000px] h-[450px] overflow-auto">           
-                  <div className="border-solid border-[1px] p-[20px]  border-black rounded-lg hover:bg-[#d9d9d9] ">
+                  <div className="border-solid border-[1px] p-[20px] border-black rounded-lg hover:bg-[#d9d9d9] ">
                       <button className="flex flex-col  w-[250px]">
                         <img src={Campus} alt="" className="lex self-center rounded-2xl w-[200px] p-[5px]"/>
                         <h2 className="text-[#194F90] text-[1.2rem] font-bold">Management Information Center</h2>
-                        
                       </button>
                     </div>
                     <div className="border-solid border-[1px] p-[20px] border-black rounded-lg hover:bg-[#d9d9d9]">
