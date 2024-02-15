bb19e562efbeda229921e82b34469042c3ba535b
Paolo Banaglorioso <93817250+chippee08@users.noreply.github.com>
2024-02-15 12:14:07 +0800

    Update SelectOffice.jsx
    medyo maayos na yung layout. pero baka ayusin ko pa pati yung responsiveness niya

--- react/src/Component/SelectOffice.jsx
+++ react/src/Component/SelectOffice.jsx
@@ -5,50 +5,52 @@ import Campus from './Subcomponent/Asset/RTU_Campus.jpg'
 function SelectOffice() {
   return (
     <>
-        <div >
-            <div className="">
+        <div className=" border-solid h-screen overflow-hidden">
+            <div className="flex flex-col ">
                 <SubNav />
                 <h1 className="flex justify-center text-[#194F90] text-[1.9rem] font-bold pb-[30px]">Select Office</h1>
+                
+                <div className="flex justify-evenly self-center flex-wrap gap-[20px] w-[1000px] h-[450px] overflow-auto">           
+                  <div className="border-solid border-[1px] p-[20px]  border-black rounded-lg hover:bg-[#d9d9d9] ">
+                      <button className="flex flex-col  w-[250px]">
+                        <img src={Campus} alt="" className="lex self-center rounded-2xl w-[200px] p-[5px]"/>
+                        <h2 className="text-[#194F90] text-[1.2rem] font-bold">Management Information Center</h2>
+                        
+                      </button>
+                    </div>
+                    <div className="border-solid border-[1px] p-[20px] border-black rounded-lg hover:bg-[#d9d9d9]">
+                      <button className="flex flex-col  w-[250px]">
+                        <img src={Campus} alt="" className="flex self-center rounded-2xl w-[200px] p-[5px]"/>
+                        <h2 className="text-[#194F90] text-[1.2rem] font-bold">Management Information Center</h2>   
+                      </button>
+                    </div>
+                    <div className="border-solid border-[1px] p-[20px] border-black rounded-lg hover:bg-[#d9d9d9]">
+                      <button className="flex flex-col  w-[250px]">
+                        <img src={Campus} alt="" className="flex self-center rounded-2xl w-[200px] p-[5px]"/>
+                        <h2 className="text-[#194F90] text-[1.2rem] font-bold">Management Information Center</h2>   
+                      </button>
+                    </div>
+                    <div className="border-solid border-[1px] p-[20px] border-black rounded-lg hover:bg-[#d9d9d9]">
+                      <button className="flex flex-col  w-[250px]">
+                        <img src={Campus} alt="" className="flex self-center rounded-2xl w-[200px] p-[5px]"/>
+                        <h2 className="text-[#194F90] text-[1.2rem] font-bold">Management Information Center</h2>   
+                      </button>
+                    </div>
+                    <div className="border-solid border-[1px] p-[20px] border-black rounded-lg hover:bg-[#d9d9d9]">
+                      <button className="flex flex-col  w-[250px]">
+                        <img src={Campus} alt="" className="flex self-center rounded-2xl w-[200px] p-[5px]"/>
+                        <h2 className="text-[#194F90] text-[1.2rem] font-bold">Management Information Center</h2>   
+                      </button>
+                    </div>
+                    <div className="border-solid border-[1px] p-[20px] border-black rounded-lg hover:bg-[#d9d9d9]">
+                      <button className="flex flex-col  w-[250px]">
+                        <img src={Campus} alt="" className="flex self-center rounded-2xl w-[200px] p-[5px]"/>
+                        <h2 className="text-[#194F90] text-[1.2rem] font-bold">Management Information Center</h2>   
+                      </button>
+                    </div>
+                  </div>  
             </div>
-            <div className="flex justify-evenly flex-wrap overflow-auto"> 
-              <div className="border-solid border-[1px] border-black rounded-lg hover:bg-[#d9d9d9] ">
-                  <button className="flex flex-col p-[20px] w-[250px]">
-                    <img src={Campus} alt="" className="lex self-center rounded-2xl w-[300px] p-[10px]"/>
-                    <h2 className="text-[#194F90] text-[1.5rem] font-bold">Management Information Center</h2>
-                    
-                  </button>
-                </div>
-                <div className="border-solid border-[1px] border-black rounded-lg hover:bg-[#d9d9d9]">
-                  <button className="flex flex-col p-[20px] w-[250px]">
-                    <img src={Campus} alt="" className="flex self-center rounded-2xl w-[300px] p-[10px]"/>
-                    <h2 className="text-[#194F90] text-[1.5rem] font-bold">Management Information Center</h2>   
-                  </button>
-                </div>
-                <div className="border-solid border-[1px] border-black rounded-lg hover:bg-[#d9d9d9]">
-                  <button className="flex flex-col p-[20px] w-[250px]">
-                    <img src={Campus} alt="" className="flex self-center rounded-2xl w-[300px] p-[10px]"/>
-                    <h2 className="text-[#194F90] text-[1.5rem] font-bold">Management Information Center</h2>   
-                  </button>
-                </div>
-                <div className="border-solid border-[1px] border-black rounded-lg hover:bg-[#d9d9d9]">
-                  <button className="flex flex-col p-[20px] w-[250px]">
-                    <img src={Campus} alt="" className="flex self-center rounded-2xl w-[300px] p-[10px]"/>
-                    <h2 className="text-[#194F90] text-[1.5rem] font-bold">Management Information Center</h2>   
-                  </button>
-                </div>
-                <div className="border-solid border-[1px] border-black rounded-lg hover:bg-[#d9d9d9]">
-                  <button className="flex flex-col p-[20px] w-[250px]">
-                    <img src={Campus} alt="" className="flex self-center rounded-2xl w-[300px] p-[10px]"/>
-                    <h2 className="text-[#194F90] text-[1.5rem] font-bold">Management Information Center</h2>   
-                  </button>
-                </div>
-                <div className="border-solid border-[1px] border-black rounded-lg hover:bg-[#d9d9d9]">
-                  <button className="flex flex-col p-[20px] w-[250px]">
-                    <img src={Campus} alt="" className="flex self-center rounded-2xl w-[300px] p-[10px]"/>
-                    <h2 className="text-[#194F90] text-[1.5rem] font-bold">Management Information Center</h2>   
-                  </button>
-                </div>
-              </div>  
+            
         </div>
     </>  
   )
