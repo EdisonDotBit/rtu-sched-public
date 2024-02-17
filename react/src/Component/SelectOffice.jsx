9d63eb8a7e7de8cdf13c358881b0966e3b8af093
Paolo Banaglorioso <93817250+chippee08@users.noreply.github.com>
2024-02-17 09:59:04 +0800

    Update SelectOffice.jsx

--- react/src/Component/SelectOffice.jsx
+++ react/src/Component/SelectOffice.jsx
@@ -10,7 +10,7 @@ function SelectOffice() {
                 <SubNav />
                 <h1 className="flex justify-center text-[#194F90] text-[1.9rem] font-bold pb-[30px]">Select Office</h1>
                 
-                <div className="flex justify-evenly self-center flex-wrap gap-[20px] w-[1000px] h-[450px] overflow-auto">           
+                <div className="flex justify-evenly self-center flex-wrap gap-[20px] w-[1000px] h-[350px] overflow-auto">           
                   <div className="border-solid border-[1px] p-[20px] border-black rounded-lg hover:bg-[#d9d9d9] ">
                       <button className="flex flex-col  w-[250px]">
                         <img src={Campus} alt="" className="lex self-center rounded-2xl w-[200px] p-[5px]"/>
