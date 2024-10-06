ecfca0b747b79cd7e2e8b16edbe7434a0809d1d5
chippee08 <paolobanag08@gmail.com>
2024-10-06 13:14:21 +0800

    as

--- react/src/Layouts/StudentLayout.jsx
+++ react/src/Layouts/StudentLayout.jsx
@@ -18,7 +18,7 @@ function StudentLayout() {
     }, []);
     return (
         <>
-            <div className="flex flex-col max-h-screen min-h-screen font-poppins overflow-x-hidden w-full">
+            <div className="flex flex-col max-h-screen min-h-screen font-roboto overflow-x-hidden w-full">
                 <div className="flex flex-1 bg-gray-100">
                     {/* Sidebar */}
                     <div
@@ -41,7 +41,7 @@ function StudentLayout() {
                                     d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                                 />
                             </svg>
-                            <span className="text-white font-bold uppercase mr-2">
+                            <span className="text-white font-roboto font-thin uppercase mr-2">
                                 STUDENT
                             </span>
                         </div>
