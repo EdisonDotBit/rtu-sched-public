a1c6940e2720a7b45908d1b133d84050e0d53f5c
Edison Lati Jr. <mirage.lati@gmail.com>
2024-10-06 13:18:42 +0800

    Update font config in tailwind.config.js
    Replace font-poppins to font-roboto in tailwind.config.js

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                poppins: ["Poppins", "sans-serif"],
+                roboto: ["Roboto","sans-serif"],
             },
             screens: {
                 "xsm": "200px",
