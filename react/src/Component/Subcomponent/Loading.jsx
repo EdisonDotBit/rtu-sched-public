f48b42f5a82083b3ddbf448b0eec0b691dccbca5
Edison Lati Jr. <mirage.lati@gmail.com>
2024-11-14 23:24:24 +0800

    Add more text in Loading.jsx

--- react/src/Component/Subcomponent/Loading.jsx
+++ react/src/Component/Subcomponent/Loading.jsx
@@ -2,7 +2,7 @@ function Loading() {
     return (
         <div className="flex flex-col justify-center items-center w-full h-full">
             <span className="loading loading-dots loading-lg"></span>
-            Loading...
+            Loading Appointment...
         </div>
     );
 }
