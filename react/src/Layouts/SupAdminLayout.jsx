9bb7842f83e0bf61d61d346ea51af9b4d2eb6021
Edison Lati Jr. <mirage.lati@gmail.com>
2024-11-17 21:57:11 +0800

    Update SupAdminLayout.jsx
    Updated SupAdminLayout.jsx by adjusting the size of sidenav, icons, and links.

--- react/src/Layouts/SupAdminLayout.jsx
+++ react/src/Layouts/SupAdminLayout.jsx
@@ -79,7 +79,7 @@ function SupAdminLayout() {
                 <div className="flex flex-1 overflow-hidden">
                     {/* Sidebar */}
                     <aside
-                        className={`fixed inset-y-0 left-0 z-30 w-72 md:w-72 bg-[#194F90] transform transition-transform duration-300 ease-in-out lg:relative lg:translate-x-0 ${
+                        className={`fixed inset-y-0 left-0 z-30 w-72 sm:w-60 bg-[#194F90] transform transition-transform duration-300 ease-in-out lg:relative lg:translate-x-0 ${
                             isSidebarOpen
                                 ? "translate-x-0"
                                 : "-translate-x-full"
@@ -126,7 +126,7 @@ function SupAdminLayout() {
                                 </svg>
                             </button>
                         </div>
-                        <nav className="flex flex-col flex-1 overflow-y-auto">
+                        <nav className="text-sm flex flex-col flex-1 overflow-y-auto">
                             <NavLink
                                 to="/ewqqwe/suppa/feedbacks"
                                 className={`relative flex items-center px-6 sm:px-8 py-4 sm:py-6 text-white hover:bg-[#123A69] ${
@@ -150,7 +150,7 @@ function SupAdminLayout() {
                                     viewBox="0 0 24 24"
                                     stroke-width="1.5"
                                     stroke="currentColor"
-                                    class="w-8 h-8 mr-1"
+                                    class="w-7 h-7 mr-1"
                                 >
                                     <path
                                         stroke-linecap="round"
@@ -158,7 +158,7 @@ function SupAdminLayout() {
                                         d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
                                     />
                                 </svg>
-                                <span className="ml-3">View Feedbacks</span>
+                                <span className="ml-4">View Feedbacks</span>
                             </NavLink>
                             <NavLink
                                 to="/ewqqwe/suppa/appointments"
@@ -183,7 +183,7 @@ function SupAdminLayout() {
                                     viewBox="0 0 24 24"
                                     strokeWidth={1.5}
                                     stroke="currentColor"
-                                    className="w-8 h-8 mr-1"
+                                    className="w-7 h-7 mr-1"
                                 >
                                     <path
                                         strokeLinecap="round"
@@ -191,7 +191,7 @@ function SupAdminLayout() {
                                         d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
                                     />
                                 </svg>
-                                <span className="ml-3">Manage Appointment</span>
+                                <span className="ml-4 ">Appointment</span>
                             </NavLink>
                             <NavLink
                                 to="/ewqqwe/suppa/offices"
@@ -216,7 +216,7 @@ function SupAdminLayout() {
                                     viewBox="0 0 24 24"
                                     stroke-width="1.5"
                                     stroke="currentColor"
-                                    class="w-8 h-8 mr-1"
+                                    class="w-7 h-7 mr-1"
                                 >
                                     <path
                                         stroke-linecap="round"
@@ -224,7 +224,7 @@ function SupAdminLayout() {
                                         d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
                                     />
                                 </svg>
-                                <span className="ml-3">Manage Offices</span>
+                                <span className="ml-4">Manage Offices</span>
                             </NavLink>
                             <NavLink
                                 to="/ewqqwe/suppa/accounts"
@@ -249,7 +249,7 @@ function SupAdminLayout() {
                                     viewBox="0 0 24 24"
                                     stroke-width="1.5"
                                     stroke="currentColor"
-                                    class="w-8 h-8 mr-1"
+                                    class="w-7 h-7 mr-1"
                                 >
                                     <path
                                         stroke-linecap="round"
@@ -257,7 +257,7 @@ function SupAdminLayout() {
                                         d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
                                     />
                                 </svg>
-                                <span className="ml-3">Admin Accounts</span>
+                                <span className="ml-4">Admin Accounts</span>
                             </NavLink>
                             <NavLink
                                 to="/ewqqwe/suppa/manage"
@@ -281,7 +281,7 @@ function SupAdminLayout() {
                                     viewBox="0 0 24 24"
                                     strokeWidth={1.5}
                                     stroke="currentColor"
-                                    className="w-8 h-8 mr-1"
+                                    className="w-7 h-7 mr-1"
                                 >
                                     <path
                                         strokeLinecap="round"
@@ -289,7 +289,7 @@ function SupAdminLayout() {
                                         d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
                                     />
                                 </svg>
-                                <span className="ml-3">My Account</span>
+                                <span className="ml-4">My Account</span>
                             </NavLink>
                         </nav>
                     </aside>
