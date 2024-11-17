3796edf0ffc7a707741368c3d96e4f709026bc0c
Edison Lati Jr. <mirage.lati@gmail.com>
2024-11-17 21:48:03 +0800

    Update EditAccount.jsx
    Updated EditAccount.jsx by adding admrole or the assigned office in edit (however, it is not editable to ensure system error or user discipline issue). Also fixed form interface.

--- react/src/Component/Admin/Component/EditAccount.jsx
+++ react/src/Component/Admin/Component/EditAccount.jsx
@@ -45,6 +45,7 @@ function EditAccount({ selectedaccid }) {
                     admuser: responseData.admuser,
                     admpass: responseData.admpass,
                     admname: responseData.admname,
+                    admrole: responseData.admrole,
                     // admempnum: responseData.admempnum,
                 });
             } catch (error) {
@@ -61,58 +62,53 @@ function EditAccount({ selectedaccid }) {
 
     return (
         <>
-            <div className="w-full h-[500px] overflow-y-auto">
-                <div className="flex flex-col justify-center">
-                    <div>
-                        <h1 className="flex justify-center text-2xl underline text-black mb-6">
-                            Edit Account Form
-                        </h1>
-                        <h1 className="flex justify-center text-xl text-black">
-                            Update Account Details {selectedaccid}
-                        </h1>
-                    </div>
-                    <div className="flex flex-col justify-center">
-                        <div className="flex justify-center items-center"></div>
-                        <div className="flex flex-col justify-center">
-                            <h1 className="flex justify-center text-3xl mb-5"></h1>
-                            <div className="flex flex-col justify-center w-full items-center">
-                                <div className="flex flex-col justify-center items-center w-3/4">
-                                    <label className="m-3 input input-bordered flex items-center gap-2 bg-gray-200 text-black border-black sm:w-2/3 md:w-8/12 lg:w-1/3">
-                                        Full Name :
-                                        <input
-                                            name="admname"
-                                            value={formData.admname}
-                                            onChange={handleChange}
-                                            type="text"
-                                            placeholder="Banaglorios Nga Pala"
-                                            className="grow"
-                                        />
-                                    </label>
-                                    {/* <label className="m-3 input input-bordered flex items-center gap-2 bg-gray-200 text-black border-black sm:w-2/3 md:w-8/12 lg:w-1/3">
-                                        Employee Number:
-                                        <input
-                                            name="admempnum"
-                                            value={formData.admempnum}
-                                            onChange={handleChange}
-                                            type="text"
-                                            placeholder="SA-D-B0-1"
-                                            className="grow"
-                                        />
-                                    </label> */}
-                                    <label className="m-3 focus:border-transparent input input-bordered flex items-center gap-2 bg-gray-200 text-black border-black sm:w-2/3 md:w-8/12 lg:w-1/3">
-                                        Username :
-                                        <input
-                                            name="admuser"
-                                            value={formData.admuser}
-                                            onChange={handleChange}
-                                            type="text"
-                                            placeholder="mapagmahalzero2"
-                                            className="grow focus:border-blue-400 text-gray-400"
-                                            disabled
-                                        />
-                                    </label>
-                                    <label className="m-3 focus:border-transparent input input-bordered flex items-center gap-2 bg-gray-200 text-black border-black sm:w-2/3 md:w-8/12 lg:w-1/3">
-                                        Password :
+            <div className="w-full h-[500px] flex justify-center">
+                <div className="w-full md:w-3/4 lg:w-[50%] border border-gray-300 bg-[#194F90] rounded-md shadow-md px-8 py-2">
+                    <div className="flex flex-col space-y-4">
+                        <div className="flex flex-col space-y-4">
+                            <h2 className="text-white text-xl font-semibold text-center mt-4">
+                                Edit Admin Account
+                            </h2>
+                            {/* Full Name */}
+                            <label className="block text-white">
+                                Admin Name:
+                                <input
+                                    className="text-gray-800 bg-white w-full mt-1 py-2 px-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#FFDB75]"
+                                    name="admname"
+                                    value={formData.admname}
+                                    onChange={handleChange}
+                                    type="text"
+                                />
+                            </label>
+                            {/* Employee Number */}
+                            {/* <label className="block text-white">
+                                Employee Number
+                                <input
+                                    className="text-gray-500 bg-white w-full mt-1 py-2 px-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#FFDB75]"
+                                    name="admempnum"
+                                    value={formData.admempnum}
+                                    onChange={handleChange}
+                                    type="text"
+                                    placeholder="SA-D-B0-1"
+                                />
+                            </label> */}
+                            {/* Username */}
+                            <label className="block text-white">
+                                Username:
+                                <input
+                                    className="text-gray-500 bg-white w-full mt-1 py-2 px-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#FFDB75]"
+                                    name="admuser"
+                                    value={formData.admuser}
+                                    onChange={handleChange}
+                                    type="text"
+                                    disabled
+                                />
+                            </label>
+                            {/* Password */}
+                            <div className="relative w-full">
+                                <label className="block text-white">
+                                    Password:
+                                    <div className="relative">
                                         <input
                                             name="admpass"
                                             value={formData.admpass}
@@ -122,34 +118,48 @@ function EditAccount({ selectedaccid }) {
                                                     ? "text"
                                                     : "password"
                                             } // Toggle between text and password type
-                                            placeholder="Bakal Pass"
-                                            className="grow focus:border-blue-400"
+                                            placeholder="Enter Password"
+                                            className="text-gray-800 bg-white w-full mt-1 py-2 px-3 pr-12 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#FFDB75]"
                                         />
                                         <button
+                                            type="button"
                                             onClick={() =>
                                                 setShowPassword(!showPassword)
                                             }
+                                            className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
                                         >
-                                            {showPassword ? "Hide" : "Show"}{" "}
-                                            {/* Toggle between "Hide" and "Show" */}
-                                        </button>
-                                    </label>
-
-                                    <div className="flex ">
-                                        <a href="#main">
-                                            <button className="btn btn-square bg-transparent text-black mr-5">
-                                                Back
-                                            </button>
-                                        </a>
-                                        <button
-                                            type="button"
-                                            className="btn btn-outline bg-yellow-500 text-black"
-                                            onClick={editAcc}
-                                        >
-                                            Edit Account
+                                            {showPassword ? "Hide" : "Show"}
                                         </button>
                                     </div>
-                                </div>
+                                </label>
+                            </div>
+
+                            {/* Assigned Office */}
+                            <label className="block text-white">
+                                Assigned office:
+                                <input
+                                    className="text-gray-500 bg-white w-full mt-1 py-2 px-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#FFDB75]"
+                                    name="admrole"
+                                    value={formData.admrole}
+                                    onChange={handleChange}
+                                    type="text"
+                                    disabled
+                                />
+                            </label>
+
+                            <div className="flex justify-center gap-6">
+                                <a href="#main">
+                                    <button className="btn btn-outline px-6 text-[#194F90] bg-[#FFDB75] hover:bg-[#f3cd64] hover:text-[#194F90] mt-2">
+                                        Back
+                                    </button>
+                                </a>
+                                <button
+                                    type="button"
+                                    className="btn btn-outline px-6 text-[#194F90] bg-[#FFDB75] hover:bg-[#f3cd64] hover:text-[#194F90] mt-2"
+                                    onClick={editAcc}
+                                >
+                                    Save
+                                </button>
                             </div>
                         </div>
                     </div>
