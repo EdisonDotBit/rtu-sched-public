a050b830052e0fa5b59be339560b0b0c0d95577b
Edison Lati Jr. <mirage.lati@gmail.com>
2024-11-18 12:24:10 +0800

    Change img in NotFound.jsx

--- react/src/Component/NotFound.jsx
+++ react/src/Component/NotFound.jsx
@@ -1,8 +1,8 @@
-import E from "./Subcomponent/Asset/e.jpg";
+import NotFoundLogo from "./Subcomponent/Asset/page-not-found.png";
 function NotFound() {
     return (
-        <div className="relative w-full h-screen">
-            <img className="w-full h-full object-cover" src={E} alt="" />
+        <div className="relative w-full h-full flex justify-center items-center">
+            <img className="w-[500px] h-auto" src={NotFoundLogo} alt="" />
             <div className="absolute inset-0 flex items-center justify-center">
                 <div className="bg-black bg-opacity-50 text-white p-4">
                     404 Not found
