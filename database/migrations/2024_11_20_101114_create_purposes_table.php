3abfd2fc4ffef5677b70db686045134f32571017
Edison Lati Jr. <mirage.lati@gmail.com>
2024-11-20 21:26:04 +0800

    Create 2024_11_20_101114_create_purposes_table.php

---
+++ database/migrations/2024_11_20_101114_create_purposes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurposesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purposes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('office_id');
+            $table->string('purpose');
+            $table->timestamps();
+            $table->foreign('office_id')->references('offid')->on('offices')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purposes');
+    }
+}
