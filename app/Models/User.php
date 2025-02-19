f9bf116409c29cf2d651d003827d15f618943958
Edison Lati Jr. <junior.lati@gmail.com>
2025-02-20 03:27:58 +0800

    Add User.php model

---
+++ app/Models/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory;
+
+    protected $fillable = [
+        'username',
+        'email',
+        'contact_number',
+        'password',
+        'role',
+        'full_name',
+        'student_number',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
