9a715963fd34680a47dbb4accc8bf36902d87aa5
Edison Lati Jr. <junior.lati@gmail.com>
2025-02-20 03:31:25 +0800

    Update AppRouter.jsx
    Updated AppRouter.jsx by adding StudentAuthProvider and components for register, authentication, and login for account-based setup.

--- react/src/AppRouter.jsx
+++ react/src/AppRouter.jsx
@@ -12,6 +12,7 @@ import AddOffice from "./Component/Admin/Component/AddOffice.jsx";
 import LoginAdmin from "./Component/Admin/LoginAdmin.jsx";
 import GuestSetAppointment from "./Component/GuestSetAppointment.jsx";
 import { AuthProvider } from "./Hooks/useAuth.jsx";
+import { StudentAuthProvider } from "./Hooks/useStudentAuth.jsx";
 import { ProtectedRoute } from "./ProtectedRoute.jsx";
 import AdminLayout from "./Layouts/AdminLayout.jsx";
 import NotFound from "./Component/NotFound.jsx";
@@ -19,148 +20,175 @@ import Feedback from "./Component/Feedback.jsx";
 import Feedbacks from "./Component/Admin/Feedbacks.jsx";
 import ManageAcc from "./Component/Admin/ManageAcc.jsx";
 import OfficeAdminManage from "./Component/Admin/OfficeAdminManage.jsx";
+import Login from "./Component/Authentication/Login.jsx";
+import Register from "./Component/Authentication/Register.jsx";
+import ProtectedStudentRoute from "./ProtectedStudentRoute.jsx";
+import Authentication from "./Component/Authentication/Authentication.jsx";
 
 const AppRouter = () => (
     <>
         <AuthProvider>
-            <Routes>
-                <Route path="feedback" element={<Feedback />} />
-                <Route path="/rtu/admin" element={<AdminLayout />}>
-                    <Route
-                        path=""
-                        element={
-                            <ProtectedRoute>
-                                <Feedbacks />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="office"
-                        element={
-                            <ProtectedRoute>
-                                <OfficeAdminManage />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="manage"
-                        element={
-                            <ProtectedRoute>
-                                <ManageAcc />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="feedback"
-                        element={
-                            <ProtectedRoute>
-                                <Feedbacks />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="appointments"
-                        element={
-                            <ProtectedRoute>
-                                <AppointmentsAdmin />
-                            </ProtectedRoute>
-                        }
-                    />
-                </Route>
-                <Route path="*" element={<NotFound />} />
-                <Route path="rtu/login" element={<LoginAdmin />} />
-                <Route path="/" element={<Navigate to="/student" />} />
-                <Route
-                    path="/rtu/suppa"
-                    element={
-                        <ProtectedRoute>
-                            <SupAdminLayout />
-                        </ProtectedRoute>
-                    }
-                >
-                    <Route
-                        path=""
-                        element={
-                            <ProtectedRoute>
-                                <Feedbacks />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="feedbacks"
-                        element={
-                            <ProtectedRoute>
-                                <Feedbacks />
-                            </ProtectedRoute>
-                        }
-                    />
+            <StudentAuthProvider>
+                <Routes>
+                    <Route path="feedback" element={<Feedback />} />
+
+                    {/* Admin Routes */}
+                    <Route path="/rtu/admin" element={<AdminLayout />}>
+                        <Route
+                            path=""
+                            element={
+                                <ProtectedRoute>
+                                    <Feedbacks />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="office"
+                            element={
+                                <ProtectedRoute>
+                                    <OfficeAdminManage />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="manage"
+                            element={
+                                <ProtectedRoute>
+                                    <ManageAcc />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="feedback"
+                            element={
+                                <ProtectedRoute>
+                                    <Feedbacks />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="appointments"
+                            element={
+                                <ProtectedRoute>
+                                    <AppointmentsAdmin />
+                                </ProtectedRoute>
+                            }
+                        />
+                    </Route>
+
+                    <Route path="*" element={<NotFound />} />
+                    <Route path="rtu/login" element={<LoginAdmin />} />
 
                     <Route
-                        path="manage"
+                        path="/rtu/suppa"
                         element={
                             <ProtectedRoute>
-                                <ManageAcc />
+                                <SupAdminLayout />
                             </ProtectedRoute>
                         }
-                    />
-                    <Route
-                        path="offices"
-                        element={
-                            <ProtectedRoute>
-                                <OfficelistAdmin />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="appointments"
-                        element={
-                            <ProtectedRoute>
-                                <AppointmentsAdmin />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="accounts"
-                        element={
-                            <ProtectedRoute>
-                                <AccountSettingsAdmin />
-                            </ProtectedRoute>
-                        }
-                    />
-                </Route>
+                    >
+                        <Route
+                            path=""
+                            element={
+                                <ProtectedRoute>
+                                    <Feedbacks />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="feedbacks"
+                            element={
+                                <ProtectedRoute>
+                                    <Feedbacks />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="manage"
+                            element={
+                                <ProtectedRoute>
+                                    <ManageAcc />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="offices"
+                            element={
+                                <ProtectedRoute>
+                                    <OfficelistAdmin />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="appointments"
+                            element={
+                                <ProtectedRoute>
+                                    <AppointmentsAdmin />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="accounts"
+                            element={
+                                <ProtectedRoute>
+                                    <AccountSettingsAdmin />
+                                </ProtectedRoute>
+                            }
+                        />
+                    </Route>
 
-                <Route path="/guest" element={<GuestLayout />}>
-                    <Route
-                        path=""
-                        element={<Navigate to="/guest/set-appointment" />}
-                    />
-                    <Route
-                        path="set-appointment"
-                        element={<GuestSetAppointment />}
-                    />
-                    <Route
-                        path="view-appointment"
-                        element={<ViewAppointments />}
-                    />
-                </Route>
+                    {/* Guest Routes */}
+                    <Route path="/guest" element={<GuestLayout />}>
+                        <Route
+                            path=""
+                            element={<Navigate to="/guest/set-appointment" />}
+                        />
+                        <Route
+                            path="set-appointment"
+                            element={<GuestSetAppointment />}
+                        />
+                        <Route
+                            path="view-appointment"
+                            element={<ViewAppointments />}
+                        />
+                    </Route>
 
-                <Route path="/test" element={<AddOffice />} />
+                    <Route path="/test" element={<AddOffice />} />
 
-                <Route path="/student" element={<StudentLayout />}>
+                    {/* Student Routes */}
+                    <Route path="/student" element={<StudentLayout />}>
+                        <Route
+                            path=""
+                            element={<Navigate to="/student/set-appointment" />}
+                        />
+                        <Route
+                            path="set-appointment"
+                            element={
+                                <ProtectedStudentRoute>
+                                    <SetAppointment />
+                                </ProtectedStudentRoute>
+                            }
+                        />
+                        <Route
+                            path="view-appointment"
+                            element={
+                                <ProtectedStudentRoute>
+                                    <ViewAppointments />
+                                </ProtectedStudentRoute>
+                            }
+                        />
+                    </Route>
+
+                    {/* Authentication Routes for Students */}
+                    <Route path="/student/login" element={<Login />} />
+                    <Route path="/student/register" element={<Register />} />
                     <Route
-                        path=""
-                        element={<Navigate to="/student/set-appointment" />}
+                        path="/student/authenticate"
+                        element={<Authentication />}
                     />
-                    <Route
-                        path="set-appointment"
-                        element={<SetAppointment />}
-                    />
-                    <Route
-                        path="view-appointment"
-                        element={<ViewAppointments />}
-                    />
-                </Route>
-            </Routes>
+                    <Route path="/" element={<Navigate to="/student" />} />
+                </Routes>
+            </StudentAuthProvider>
         </AuthProvider>
     </>
 );
