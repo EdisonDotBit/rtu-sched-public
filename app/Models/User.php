4b6d76280e53b93206e4644d486ea5adb1c86d78
Edison Lati Jr. <junior.lati@gmail.com>
2025-02-26 02:14:27 +0800

    Update User.php
    Updated User.php by adding verification_pin and is_verified column.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
         'username',
@@ -18,10 +19,16 @@ class User extends Authenticatable
         'role',
         'full_name',
         'student_number',
+        'verification_pin',
+        'is_verified'
     ];
 
     protected $hidden = [
-        'password',
+        // 'password',
         'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }
