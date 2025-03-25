0aff7838aa43130eb9e009aebaf141a519014466
Edison Lati Jr. <junior.lati@gmail.com>
2025-03-26 06:15:09 +0800

    Add font-roboto in App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,7 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 function App({ children }) {
     return (
-        <div className="flex flex-col min-h-screen bg-gray-100">
+        <div className="font-roboto flex flex-col min-h-screen bg-gray-100">
             {children} {/* Render the child components (e.g., Routes) */}
             <ToastContainer />
         </div>
